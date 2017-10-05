f2cb9cbcbdb89ce311f8bdb42b250c82ab662799
alteridem <julianacg1@yahoo.com>
2017-10-05 15:55:01 -0400

    Create lightMyNav.js

---
+++ lightMyNav.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+var $navLinks = $('#main-nav > dd > a');
+var $subpages = $($(".subpage-item").get().reverse());
+
+var subpageIdTonavLink = {};
+$subpages.each(function() {
+	var id = $(this).attr('id');
+	subpageIdTonavLink[id] = $('#main-nav > dd > a[href=#' + id + ']');
+});
+
+$(window).scroll(highlightNav);
+
+function highlightNav() {
+	var scrollPos = $(window).scrollTop();
+
+	$subpages.each(function() {
+		var currSubpage = $(this);
+		var subpageTop = currSubpage.offset().top();
+
+		if (scrollPos >= subpageTop) {
+			var id = currSubpage.attr('id');
+
+			var $navLink = subpageIdTonavLink[id];
+
+			if (!$navLink.hasClass('active')) {
+				$navLinks.removeClass('active');
+				$navLink.addClass('active');
+			}
+		return false;
+		}
+	});
+}
+	
+});
