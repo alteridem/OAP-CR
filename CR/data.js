5fb85765a5a4a395466171f154dcb89a1a4be1ef
alteridem <julianacg1@yahoo.com>
2017-10-16 17:29:52 -0400

    Update data.js
    Added UCLA Prep to CR - may need further tweaking

--- CR/data.js
+++ CR/data.js
@@ -361,7 +361,6 @@ var jsonObject = [
 	"location" : "New York City",
 	"grade": [9, 10, 11, 12],
 	"courses" : [
-		 {"name": "NYCS Courses", "FIELD2": "New York College Seminar", "FIELD3": "HISTORY/The Ancient World", "FIELD4": "Creation, Fine Art, Design, Perfomance", "FIELD5": "BUSINESS AND ECONOMICS", "FIELD6": "Computer Science, Robotics, and Technology", "FIELD7": "Law, Society, and International Affairs", "FIELD8": "LITERATURE AND WRITING", "FIELD9": "MEDICINE", "FIELD10": "PHILOSOPHY", "FIELD11": "SCIENCE AND THE UNIVERSE", "FIELD12": "Leadership", "FIELD13": "THE MIND", "FIELD14": "Most Popular", "FIELD15": "For Students with 0-2 years of French", "FIELD16": "For Students with 3 or more years of French", "FIELD17": "For students with 0-2 years of Spanish", "FIELD18": "For students with 3 or more years of Spanish"},
 		 {"name": "Business ", "FIELD3": "", "FIELD4": "", "FIELD5": "x", "FIELD6": "", "FIELD7": "x", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "", "FIELD14": "x", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
 		 {"name": "Human Rights Law ", "FIELD3": "x", "FIELD4": "", "FIELD5": "", "FIELD6": "", "FIELD7": "x", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "x", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
 		 {"name": "International Relations ", "FIELD3": "x", "FIELD4": "", "FIELD5": "", "FIELD6": "", "FIELD7": "x", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
@@ -369,5 +368,18 @@ var jsonObject = [
 		 {"name": "Psychology ", "FIELD3": "", "FIELD4": "", "FIELD5": "", "FIELD6": "", "FIELD7": "", "FIELD8": "", "FIELD9": "x", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "x", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
 		 {"name": "Public Speaking (Workshop only)", "FIELD3": "", "FIELD4": "x", "FIELD5": "", "FIELD6": "", "FIELD7": "x", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""}
 				]
+},
+{
+	"progName" : "Oxbridge at UCLA Prep",
+	"progUrl" : "Oxbridge-at-UCLA-Prep",
+	"location" : "Los Angeles",
+	"grade": [7, 8],
+	"courses" : [
+		 {"name": "Creative Writing", "FIELD3": "", "FIELD4": "x", "FIELD5": "", "FIELD6": "", "FIELD7": "", "FIELD8": "x", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
+		 {"name": "Photography, Design and New Media", "FIELD3": "", "FIELD4": "x", "FIELD5": "", "FIELD6": "x", "FIELD7": "", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
+		 {"name": "Speech and Debate", "FIELD3": "", "FIELD4": "x", "FIELD5": "", "FIELD6": "", "FIELD7": "", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "", "FIELD12": "x", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""},
+		 {"name": "Virtual Reality/ Artificial Intelligence", "FIELD3": "", "FIELD4": "", "FIELD5": "", "FIELD6": "x", "FIELD7": "", "FIELD8": "", "FIELD9": "", "FIELD10": "", "FIELD11": "x", "FIELD12": "", "FIELD13": "", "FIELD14": "", "FIELD15": "", "FIELD16": "", "FIELD17": "", "FIELD18": ""}
+				]
+
 }
 ]
