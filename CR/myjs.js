bf6e65c63c229704c15d2bc9783fe37e0ae28442
alteridem <julianacg1@yahoo.com>
2017-11-29 20:19:15 -0500

    Update myjs.js
    changed 7/8th grade error logic

--- CR/myjs.js
+++ CR/myjs.js
@@ -120,10 +120,10 @@
 		});
 		
 		if (textResults === "") {
-			if (selectedGrade === 8) {
-				$("#display-results").html("8th graders can attend our Oxford and Cambridge Prep Programs in Europe and our Oxbridge at UCLA Prep Program in the United States.");
-			} else if (selectedGrade === 7) {
+			if (selectedGrade === 7) {
 				$("#display-results").html("7th graders can attend our Oxbridge at UCLA Prep program.");
+			} else if (selectedGrade === 8) {
+				$("#display-results").html("8th graders can attend our Oxford and Cambridge Prep Programs in Europe and our Oxbridge at UCLA Prep Program in the United States.");
 			} else if (langResults != "") {
 				$("#display-results").html(langResults);
 			} else {
