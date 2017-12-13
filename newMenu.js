1751abe6bbc4839e5c25a91a54767bc53e965be5
alteridem <julianacg1@yahoo.com>
2017-12-13 14:02:27 -0500

    Create newMenu.js
    attempt 1

---
+++ newMenu.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+$('#bottom-nav .center').html('<div class="dropdownY">
+			<a href="/About-Oxbridge" class="btn-white reverse dropbtnY">About Oxbridge</a>
+			<div class="dropdown-contentY">
+				<a href="/About-Oxbridge#subpage-item-1">Why Choose Oxbridge</a>
+				<a href="/About-Oxbridge#subpage-item-2">Our Founder</a>
+				<a href="/About-Oxbridge#subpage-item-3">Academic Philosophy</a>
+				<a href="/About-Oxbridge#subpage-item-4">Programs & Courses</a>
+				<a href="/About-Oxbridge#subpage-item-5">Faculty</a>
+				<a href="/About-Oxbridge#subpage-item-6">FAQs</a>
+			</div>
+		</div>
+		<div class="dropdownY">
+			<a href="/Our-Programs" class="btn-white reverse dropbtnY">Our Programs</a>
+			<div class="dropdown-contentY">
+				<a href="/Our-Programs#subpage-item-1">About Oxbridge</a>
+				<a href="/Our-Programs#subpage-item-2">Programs</a>
+				<a href="/Our-Programs#subpage-item-3">Courses</a>
+				<a href="/Our-Programs#subpage-item-4">Student Life</a>
+				<a href="/Our-Programs#subpage-item-5">Student Safety & Supervision</a>
+				<a href="/Our-Programs#subpage-item-6">Programs Blogs</a>
+				<a href="/Our-Programs#subpage-item-7">Tuition, Dates, & Details</a>
+			</div>
+		</div>
+		<div class="dropdownY">
+			<a class="btn-white reverse dropbtnY">Prospective Students</a>
+			<div class="dropdown-contentY">
+				<a href="/Apply-Now">How to Apply</a>
+				<a href="/Download-Brochure">Request a Brochure</a>
+				<a href="/Info-Sessions">Attend an Info Session</a>
+				<a href="/Testimonials">Testimonials</a>
+				<a href="/Apply-Now#subpage-item-4">Financial Aid</a>
+				<a href="/Contact-Us">Contact Us</a>
+			</div>
+		</div>
+		<div class="dropdownY">
+			<a class="btn-white reverse dropbtnY">Educators</a>
+			<div class="dropdown-contentY">
+				<a href="/Educator-Journey#subpage-item-1">Programs for Educators</a>
+				<a href="/Educator-Journey#subpage-item-2">Courses for Educators</a>
+				<a href="/Educator-Journey#subpage-item-3">Programs for School Groups</a>
+			</div>
+		</div>');
+
+});
